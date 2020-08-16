84cb2e311d4f449ca705d15eb8259e8db0c162c3
Charles <charles.gauvin123@gmail.com>
2020-08-16 12:01:30 -0400

    added a new 2D kde fct + fct for shp donwload

---
+++ R/downloadShp.R
@@ -0,0 +1,107 @@
+
+#' Fct that wraps st_read with a curl call
+#' 
+#' Can read geojsons for instance
+#' 
+#' Tries to download the file to dirToDownload/nameOfFile for persistance.
+#' If this fails, it just downloads it to a tmp file. 
+#' If dirToDownload/nameOfFile already exists, then it just reads back the shp file.
+#' nameOfFile is created from the url path
+#'
+#' @param url 
+#' @param dirToDownload 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+st_read_remote <- function(url, dirToDownload= here('Data')){
+  
+  require(glue)
+  require(here)
+  require(sf)
+  require(httr)
+  require(curl)
+  
+
+  destFile <- tryCatch({
+    urlPath <- parse_url(url)$path
+    
+    splitUrl <- strsplit(urlPath, split='/')
+    destFile <- file.path(dirToDownload, splitUrl[[1]][[length(splitUrl[[1]])]])
+  },error=function(e){
+    print(glue("Fatal error with url {url}\n{e}\nsaving to root: {here()}"))
+    return( tempfile()  )
+  })
+
+  #Check if file already exists
+  if(file.exists(destFile)){
+    print('Shp file already exists...')
+    return( st_read(destFile) )
+  }
+  
+  print('Downloading shp file...')
+  
+  #If not download it
+  curl_download(url = url,
+                destfile = destFile )
+  
+
+  
+  return( st_read(destFile) )
+  
+  
+}
+
+get_zipped_remote_shapefile <- function(path){
+  
+  require(glue)
+  require(here)
+  require(sf)
+  require(httr)
+  require(curl)
+  
+  h <- new_handle()
+  handle_setopt(h, ssl_verifyhost = 0, ssl_verifypeer=0)
+  
+
+  dest_dir = here("Data")
+  pathRootNoExt <- tools::file_path_sans_ext( basename(path)  )
+  
+  if(grepl('http',path)){
+    
+    #Create the Data/subdir if required
+    GeneralHelpers::createDirIfNotExistentRecursive(here("Data",pathRootNoExt))
+    
+    #Download the file
+    dest <- file.path(dest_dir, pathRootNoExt, glue("{pathRootNoExt}.zip") )
+    
+    #Check if the shp exists
+    shpPath <- file.path(dest_dir, pathRootNoExt, glue("{pathRootNoExt}.shp|geojson") ) 
+    if ( file.exists( shpPath  )){
+      return (  st_read(shpPath,quiet=TRUE) )
+    }
+    
+    curl_download(url= path, 
+                  destfile = dest , 
+                  handle = h)
+    path <- dest
+  }
+  
+  if(grepl('.tgz$|.tar.gz$',path)){
+    utils::untar(path, exdir = file.path(dest_dir, pathRootNoExt))
+  } else if(grepl('.zip$',path)){
+    utils::unzip(path, exdir = file.path(dest_dir, pathRootNoExt))
+  } else{
+    stop('Unsupported filetype')
+  }
+  
+  file.remove(path)
+  
+  shape_name = grep('.shp|geojson',list.files(dest_dir),value=T)
+  setwd(dest_dir)
+  sf::st_read(shape_name,quiet=TRUE)
+}
+
+
+
---
+++ R/kde.R
@@ -0,0 +1,49 @@
+
+#' Produce a 2D kernel density estimate based on some POINTS geometry
+#'
+#' Note: Taken from https://github.com/r-spatial/sf/issues/1201
+#'
+#' @param points sf object with POINT geometry
+#' @param cellsize
+#' @param bandwith
+#' @param extent allows cropping to a specific subregion of the map
+#'
+#' @return raster
+#' @export
+#'
+#' @examples \dontrun{
+#' librarry(sf)
+#' libary(here)
+#' library(magrittr)
+#' library(dplyr)
+#'
+#' shpBuildings <- st_read(dsn=here::here('Data', 'GeoData', 'QuebecCityOpenData', 'Buildings','vdq-batiments.shp'))
+#' shpCentroids <- shpBuildings %>% st_centroid
+#'
+#' rasterKDECentroids <- st_kde(shpCentroids,0.01,0.01)
+#' plot(rasterKDECentroids)
+#' }
+st_kde <- function(points,cellsize, bandwith, extent = NULL){
+
+  require(MASS)
+  require(raster)
+  require(sf)
+
+  if(is.null(extent)){
+    extent_vec <- st_bbox(points)[c(1,3,2,4)]
+  } else{
+    extent_vec <- st_bbox(extent)[c(1,3,2,4)]
+  }
+
+  n_y <- ceiling((extent_vec[4]-extent_vec[3])/cellsize)
+  n_x <- ceiling((extent_vec[2]-extent_vec[1])/cellsize)
+
+  extent_vec[2] <- extent_vec[1]+(n_x*cellsize)-cellsize
+  extent_vec[4] <- extent_vec[3]+(n_y*cellsize)-cellsize
+
+  coords <- st_coordinates(points)
+  matrix <- kde2d(coords[,1],coords[,2],h = bandwith,n = c(n_x,n_y),lims = extent_vec)
+  raster(matrix)
+}
+
+

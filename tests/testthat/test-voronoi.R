c7d788f812d1c5e809653628ec8da42e2a8329f3
Charles <charles.gauvin123@gmail.com>
2020-08-26 13:27:21 -0400

    some more figures produced in the tests (should be in a vignette)

--- tests/testthat/test-voronoi.R
+++ tests/testthat/test-voronoi.R
@@ -6,6 +6,8 @@ test_that("voronoi interpolation based on heightmax for 2 discontiguous neighbou
 
 
   mypwd <- keyringr::decrypt_gk_pw("user charles")
+  mapBoxToken <- keyringr::decrypt_gk_pw('token mapToken')
+  Sys.setenv(MAPBOX_ACCESS_TOKEN=mapBoxToken)
 
   #mtl_buildings
   conn <- DBI::dbConnect( drv = RPostgreSQL::PostgreSQL(),
@@ -35,10 +37,21 @@ test_that("voronoi interpolation based on heightmax for 2 discontiguous neighbou
   shpVoronoi <- getVoronoiInterpolation(shpBuildings,
                           'heightmax')
 
-  ggplot() +
-    geom_sf(data=shpVoronoi, aes(fill=heightmax)) +
-    geom_sf(data=shpBuildings %>% sf::st_centroid(), lwd=0.5) +
-    viridis::scale_fill_viridis()
+  p <- ggplot() +
+    snapbox::layer_mapbox( map_style = stylebox::mapbox_light(),  area = st_bbox(shpVoronoi), scale_ratio = 0.25) +
+    geom_sf(data=shpVoronoi, aes(fill=heightmax), alpha=0.2, , lwd=0.1) +
+    geom_sf(data=shpBuildings %>% sf::st_centroid(), lwd=0.2, alpha=0.2) +
+    viridis::scale_fill_viridis() +
+    ggtitle('Building maximum height') +
+    labs(subtitle="Villeray and Peter McGill",
+         caption = 'Source: NrCan and City of Montreal Lidar buildings dataset.\nMapbox base layer from {snapbox}')
+
+  ggsave(here("Figures", "voronoi_interpolate_villeray_petermcgill_heightmax_4326.png"),
+         p,
+         width = 10,
+         height = 10)
+
+
 })
 
 
@@ -154,7 +167,7 @@ test_that("voronoi interpolation based on elevmin for only 1 neighbourhoods", {
 
 
 
-test_that("voronoi interpolation based on elevmin for only 1 neighbourhoods with projected coordinates", {
+test_that("voronoi interpolation based on heightmax for only 1 neighbourhoods with projected coordinates", {
 
 
   library(ggplot2)
@@ -178,7 +191,7 @@ test_that("voronoi interpolation based on elevmin for only 1 neighbourhoods with
               	WHERE q_socio LIKE \'Villeray\'
               	) as neigh
               JOIN
-              	(SELECT elevmin, st_transform(geom, 2950) as geom
+              	(SELECT heightmax, st_transform(geom, 2950) as geom
               	FROM mtl_buildings
               	WHERE  St_isvalid (geom)
               	 )as buildings
@@ -189,11 +202,23 @@ test_that("voronoi interpolation based on elevmin for only 1 neighbourhoods with
 
 
   shpVoronoi <- getVoronoiInterpolation(shpBuildings,
-                                        'elevmin')
+                                        'heightmax')
+
 
   p <- ggplot() +
-    geom_sf(data=shpVoronoi, aes(fill=elevmin)) +
-    geom_sf(data=shpBuildings %>% sf::st_centroid(), lwd=0.5) +
-    viridis::scale_fill_viridis()
+    snapbox::layer_mapbox( map_style = stylebox::mapbox_light(),  area = st_bbox(shpVoronoi), scale_ratio = 0.25) +
+    geom_sf(data=shpVoronoi, aes(fill=heightmax), alpha=0.2, lwd=0.1) +
+    geom_sf(data=shpBuildings %>% sf::st_centroid(), lwd=0.1, alpha=0.2 ) +
+    viridis::scale_fill_viridis() +
+    ggtitle('Building maximum height') +
+    labs(subtitle="Villeray, Montreal",
+         caption = 'Source: NrCan and City of Montreal Lidar buildings dataset.\nMapbox base layer from {snapbox}\nInterpolation based on voronoi diagram of centroids')
+
+  ggsave(here("Figures", "voronoi_interpolate_villeray_heightmax_2950.png"),
+         p,
+         width = 10,
+         height = 10)
+
+
 })
 

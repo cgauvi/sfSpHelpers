47502cb52988d73e62832186e2c2dcb2128022d9
Charles <charles.gauvin@lacapitale.com>
2022-12-22 12:27:46 -0500

    fix: read.csv instead of read_csv: error with conversions and encoding probably in tests

--- tests/testthat/test-st_kde.R
+++ tests/testthat/test-st_kde.R
@@ -16,7 +16,7 @@ test_that("polygons heatmap with sf", {
 
   shp_trees <- sf::st_read('https://www.donneesquebec.ca/recherche/dataset/bc5afddf-9439-4e96-84fb-f91847b722be/resource/bbdca0dd-82df-42f9-845b-32348debf8ab/download/vdq-arbrepotentielremarquable.geojson')
 
-  shp_polyons <- get_polygon_heatmap(shp_trees , bw=.001, gsize=500 )
+  shp_polyons <- get_polygon_heatmap(shp_trees, bw=.001, gsize=500)
 
   #Make sure there are associated features and at least 2 levels
   expect_true(nrow(shp_polyons) > 1)
@@ -33,7 +33,7 @@ test_that("polygons heatmap with sf", {
 test_that("polygons heatmap with dataframe", {
 
   csv_url <-"https://data.montreal.ca/dataset/f170fecc-18db-44bc-b4fe-5b0b6d2c7297/resource/c7d0546a-a218-479e-bc9f-ce8f13ca972c/download/localisation_des_compteurs_velo.csv"
-  shp_bike_counting_stations <-   readr::read_csv(csv_url)
+  shp_bike_counting_stations <-  read.csv(csv_url)
 
   shp_bike_counting_stations %<>% dplyr::mutate(lat=Latitude)
   shp_bike_counting_stations %<>% dplyr::mutate(lng=Longitude)
